6e9decbdcb1a965e58e9e1c7092a854e85fed150
shahinul22 <shahinul.alam007@gmail.com>
2025-05-25 03:30:03 +0600

    pp

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views 
 from user.views import profile, admin_login 
-
+from .views import home
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("login/", admin_login, name="admin_login"),
+    path("", home, name="home"),
+    path("superAdmin/login/", admin_login, name="admin_login"),
     path("profile/", profile, name="profile"),
     # path("profile/<int:user_id>/", profile, name="profile_detail"),
 ]
